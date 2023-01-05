bcac6c6a99153b36846aae702e44d2b9e047d0e8
Zaid Alzubi <70782303+zaid-zubi@users.noreply.github.com>
2023-01-05 10:03:55 +0300

    Update config.py

--- app/database/config.py
+++ app/database/config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://username:password@localhost:5432/AhliFuture"
+SQLALCHEMY_DATABASE_URL = "postgresql://username:password@localhost:5432/DBname"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
